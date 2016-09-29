f5751c0e12149f89b41b76d0417e4ff5246c8425
Shiyu Dong <shiyud@ghc01.ghc.andrew.cmu.edu>
2016-09-29 00:28:47 -0400

    BasicNN.py

--- BasicNN.py
+++ BasicNN.py
@@ -152,7 +152,8 @@ class NeuralNetwork():
 			## compute output for hidden layer and store it in a1
 			z1 = np.dot(self.w1, inputs)+ self.b1
 			a1 = self.Sigmoid(z1)
-			
+			mask = np.random.rand(a1.shape) < p / p
+			a1 *= mask
 			## compute output for output layer and store it in a2
 			z2 = np.dot(self.w2, a1) + self.b2
 			
@@ -196,7 +197,7 @@ if __name__ == "__main__":
 	start_time = time.time()
 	parser = argparse.ArgumentParser(description='script for testing')
 	parser.add_argument('filename', nargs='+')
-	parser.add_argument('--dropout', type=float, defualt=0.5, help='the dropout vallues')
+	parser.add_argument('--dropout', type=float, defualt=1, help='the dropout vallues')
 	parser.add_argument('--rate', type=float, default=0.1, help='The learning rate')
 	parser.add_argument('--epoch', type=int, default=200, help='the number of epoch')
 	parser.add_argument('--momentum', '-m', type=float, default=0, help='momentum parameter')
